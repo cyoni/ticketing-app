c2390b7cdb9aae1d8cfc4096bfcfd72628c484c2
Yoni <cyoni10@gmail.com>
2024-07-26 12:14:17 +0300

    updates

--- common/build/middlewares/validate-request.js
+++ common/build/middlewares/validate-request.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateRequest = void 0;
 var express_validator_1 = require("express-validator");
-var request_validation_error_1 = require("../../common/errors/request-validation-error");
+var request_validation_error_1 = require("../errors/request-validation-error");
 var validateRequest = function (req, res, next) {
     var errors = (0, express_validator_1.validationResult)(req);
     if (!errors.isEmpty()) {
