ec7c49971f1f7d436b75b4cb8c0a3f8f804e7d72
Yoni <cyoni10@gmail.com>
2024-07-26 12:14:11 +0300

    updates

--- common/build/middlewares/error-handler.js
+++ common/build/middlewares/error-handler.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.errorHandler = void 0;
-var customError_1 = require("../../common/errors/customError");
+var customError_1 = require("../errors/customError");
 var errorHandler = function (err, req, res, next) {
     if (err instanceof customError_1.CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
--- common/build/middlewares/require-auth.middleware.js
+++ common/build/middlewares/require-auth.middleware.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RequireAuth = void 0;
-var common_1 = require("common");
+var not_authorized_error_1 = require("../errors/not-authorized-error");
 var RequireAuth = function (req, res, next) {
     console.log("req.currentUser", req.currentUser);
     if (!req.currentUser) {
-        return next(new common_1.NotAuthorizedError());
+        return next(new not_authorized_error_1.NotAuthorizedError());
     }
     next();
 };
